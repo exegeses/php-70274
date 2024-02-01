863b783da8f4d3c2bcf2d0951589b823ec6ba9c2
Marcos <exegeses@gmail.com>
2024-02-01 19:37:17 -0300

    Aplicación de alta de un producto

---
+++ curso/catalogo/agregarProducto.php
@@ -0,0 +1,30 @@
+<?php
+    //require 'config/config.php';
+    require 'funciones/conexion.php';
+    require 'funciones/productos.php';
+    $check = agregarProducto();
+    include 'layouts/header.php';
+    include 'layouts/nav.php';
+?>
+
+    <main class="container py-4">
+        <h1>Alta de un producto</h1>
+
+<?php
+        $mensaje = 'No se pudo agregar el producto';
+        $css = 'danger';
+        if( $check ) {
+            $mensaje = 'Producto agregado correctamente';
+            $css = 'success';
+        }
+?>        
+        <div class="alert alert-<?= $css ?> p-4 col-8 mx-auto shadow">
+            <?= $mensaje ?>
+            <a href="adminProductos.php" class="btn btn-dark sep">
+                volver al panel
+            </a>
+        </div>
+
+    </main>
+
+<?php  include 'layouts/footer.php';  ?>
--- curso/catalogo/funciones/productos.php
+++ curso/catalogo/funciones/productos.php
@@ -12,3 +12,53 @@
         $resultado = mysqli_query( $link, $sql );
         return $resultado;
     }
+
+    function subirImagen() : string
+    {
+        //si no enviaron archivo
+        $prdImagen = 'noDisponible.svg';
+
+        //si enviaron archivo && todo ok
+        if( $_FILES['prdImagen']['error'] == 0 ){
+            $dir = 'productos/';
+            $nombreOriginal = $_FILES['prdImagen']['name'];
+            $ext = pathinfo( $nombreOriginal, PATHINFO_EXTENSION ); // extension del archivo
+            $tmp = $_FILES['prdImagen']['tmp_name'];
+            ### renombramnos archivo
+            $prdImagen = time().'.'.$ext;
+            ### movemos archivo
+            move_uploaded_file( $tmp, $dir.$prdImagen );
+        }
+
+        return $prdImagen;
+    }
+
+    function agregarProducto() : bool
+    {
+        $prdNombre = $_POST['prdNombre'];
+        $prdPrecio = $_POST['prdPrecio'];
+        $idMarca = $_POST['idMarca'];
+        $idCategoria = $_POST['idCategoria'];
+        $prdDescripcion = $_POST['prdDescripcion'];
+        $prdImagen = subirImagen();
+
+        $link = conectar();
+        $sql = "INSERT INTO productos
+                    VALUE (
+                        DEFAULT,
+                        '".$prdNombre."',
+                        ".$prdPrecio.",
+                        ".$idMarca.",
+                        ".$idCategoria.",
+                        '".$prdDescripcion."',
+                        '".$prdImagen."',
+                        DEFAULT
+                    )";
+        try {
+            return  mysqli_query($link, $sql);
+        }
+        catch ( Exception $e ){
+            echo $e->getMessage();
+            return  false;
+        }
+    }
