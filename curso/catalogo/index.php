670934bc31c45772a9642ef3bf7b135d58e991d6
Marcos <exegeses@gmail.com>
2024-02-15 22:07:02 -0300

    Formulario para buscador de productos

--- curso/catalogo/index.php
+++ curso/catalogo/index.php
@@ -10,6 +10,27 @@
     <main class="container py-4">
         <h1>Catálogo de productos</h1>
 
+        <!-- buscador de productos -->
+        <div class="row g-3">
+            <div class="col-sm-7">
+                <input type="text" name="search" class="form-control" placeholder="Nombre de producto">
+            </div>
+            <div class="col-sm">
+                <select name="idMarca" class="form-select">
+                    <option value="">Todas las Marcas</option>
+                </select>
+            </div>
+            <div class="col-sm">
+                <select name="idCategoria" class="form-select">
+                    <option value="">Todas las Categoría</option>
+                </select>
+            </div>
+        </div>
+        <div class="row g-3 mt-1">
+            <button class="btn btn-dark">buscar</button>
+        </div>
+        <!-- fin buscador de productos -->
+
         <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4 py-5">
     <?php
         while ( $producto = mysqli_fetch_assoc( $productos ) ){
